e0afe80eb8a6df215913c7caeb128a605305e7fd
Linh <linh@linhs-mbp.lan>
2021-07-11 09:32:30 -0700

    remove unncessary function from app.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,8 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-
 import './App.css';
-import { fetchData } from './data';
 import Search from './screens/Search';
 
 
 function App() {
-
-  useEffect(() => {
-    fetchData("https://api.github.com/search/issues?q=repo:ltphan/meditation-chrome-extension is:issue is:pr")
-  })
-
   return (
     <div className="App">
       <header className="App-header">
