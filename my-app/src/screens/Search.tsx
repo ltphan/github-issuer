d883f579055e365a53991540dddec766622f9987
Linh <linh@linhs-mbp.lan>
2021-07-11 14:25:45 -0700

    fetch issues on submit for search bar

--- my-app/src/screens/Search.tsx
+++ my-app/src/screens/Search.tsx
@@ -1,12 +1,27 @@
-import { FunctionComponent } from "react"
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from "react"
+import { fetchData, urlBuilder } from "../data"
 
 const Search: FunctionComponent = () => {
+    const [value, setValue] = useState("")
+    const [issues, setIssues] = useState([])
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+    }
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const url = urlBuilder(value)
+        const issues = await fetchData(url)
+        setIssues(issues)
+    }
+
     return (
         <div>
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit}>
             <label>
                 Github View Issuer
-            <input type="text" value={"linh"} onChange={() => {}} />
+            <input type="text" value={value} onChange={handleChange} />
             </label>
             <input type="submit" value="Submit" />
         </form>
