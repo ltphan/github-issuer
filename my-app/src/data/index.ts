271de6e2dc200270953d6c0119f35b2c91feb96b
Linh <linh@linhs-mbp.lan>
2021-07-11 14:24:50 -0700

    encoded github url with correct query string parameters

--- my-app/src/data/index.ts
+++ my-app/src/data/index.ts
@@ -2,17 +2,17 @@ export function urlBuilder(url: string) {
     const stringArr = url.split('/')
     const org = stringArr[3]
     const repo = stringArr[4]
-    const queryParam = org.concat('/', repo)
-    const githubUrl = `https://api.github.com/search/issues?q=repo:${queryParam} is:issue is:pr`
+    const orgRepo = org.concat('/', repo)
+    const queryString = 'q=' + encodeURIComponent(`repo:${orgRepo} is:issue is:pr`)
+    const githubUrl = `https://api.github.com/search/issues?${queryString}`
     return githubUrl 
 }
 
 export async function fetchData(githubUrl: string) {
   try {
     const response = await fetch(githubUrl)
-    const data = response.json()
-    console.log("data", data)
-    return data
+    const data = await response.json()
+    return data.items
   } catch (e) {
     throw new Error("Error fetching data from Github API")
   }
