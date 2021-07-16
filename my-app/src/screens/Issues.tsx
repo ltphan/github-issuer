20ecb1375b7b3a8f7542a67a34caab42c0b58d26
Linh <linh@Linhs-MacBook-Pro.local>
2021-07-16 09:56:34 -0700

    list filtered based on onfilter method

--- my-app/src/screens/Issues.tsx
+++ my-app/src/screens/Issues.tsx
@@ -12,32 +12,29 @@ interface State {
 
 const Issues: FunctionComponent = () => {
     const [issues, setIssues] = useState<Array<any> | []>([])
+    const [filteredIssues, setFilteredIssues] = useState<Array<any> | []>([])
     const location  = useLocation<{ state: State}>()
     const { state } = location
         
     useEffect(() => {
         setIssues(state.state.issues)
-    }, [issues])
+    }, [state.state.issues])
 
     const onFilter = (issues: Array<any>, issueState: string) => {
-        if (issueState === "all") {
-            return
-        }
-
         const filteredIssues = issues.filter((issue) => {
             return issue.state === issueState
         })
-        setIssues(filteredIssues)
+        setFilteredIssues(filteredIssues)
     }
 
     return ( 
         <div className="container">
             <Header url={state.state.value} />
-            <button onClick={() => onFilter(issues, strings.all)}>{strings.allIssues}</button>
+            <button onClick={() => setFilteredIssues(issues)}>{strings.allIssues}</button>
             <button onClick={() => onFilter(issues, strings.open)}>{strings.openIssues}</button>
             <button onClick={() => onFilter(issues, strings.closed)}>{strings.closedIssues}</button>
             <div className="list">
-                <IssuesList issues={state.state.issues}/>
+                <IssuesList issues={filteredIssues}/>
             </div>
         </div>
     )
