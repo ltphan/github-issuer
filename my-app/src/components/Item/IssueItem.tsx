39c6625dba50e2029be22640533960b9df4e4c57
Linh <linh@linhs-mbp.lan>
2021-07-19 14:02:23 -0700

    add state enum in issue item component

--- my-app/src/components/Item/IssueItem.tsx
+++ my-app/src/components/Item/IssueItem.tsx
@@ -1,17 +1,30 @@
 import React, { FunctionComponent } from "react"
 
+import { strings } from "../../localization/strings"
+import "../Item/IssueItem.css"
+
+enum State {
+    Closed = "closed",
+    PR = "pr"
+}
+
 interface Props {
     title: string
     body: string
+    state: State
 }
 
 const IssueItem: FunctionComponent<Props> = (props) => {
-    const { title, body } = props
+    const { title, body, state } = props
 
     return (
         <div>
             <h1>{title}</h1>
-            <h1>{body}</h1>
+            {state === State.Closed && <div className="close" >&#10540;</div>}
+            {state === State.PR && <div className="close">&#9732;</div>}
+            <div>{body ? body : 
+                <span className='emptyDescription'>{strings.noDescpritionProvided} </span>}
+            </div>
         </div>
     )
 }
